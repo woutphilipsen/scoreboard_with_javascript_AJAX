ccf3c991fb2143a5371fe50b32ee83cd22316903
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2020-02-20 13:45:59 +0100

    12. immediateley invoked function expression

---
+++ web/assets/js/RepLogApp.js
@@ -0,0 +1,73 @@
+(function() {
+    var RepLogApp = {
+        initialize: function($wrapper) {
+            this.$wrapper = $wrapper;
+            Helper.initialize($wrapper);
+
+            this.$wrapper.find('.js-delete-rep-log').on(
+                'click',
+                this.handleRepLogDelete.bind(this)
+            );
+
+            this.$wrapper.find('tbody tr').on(
+                'click',
+                this.handleRowClick.bind(this)
+            );
+
+        },
+
+        updateTotalWeightLifted: function() {
+            this.$wrapper.find('.js-total-weight').html(
+                Helper.calculateTotalWeight()
+            );
+        },
+
+        handleRepLogDelete: function(e) {
+            e.preventDefault();
+
+            var $link = $(e.currentTarget);
+            $link.addClass('text-danger');
+            $link.find('.fa')
+                .removeClass('fa-trash')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
+
+            var deleteUrl = $link.data('url');
+            var $row = $link.closest('tr');
+            var self = this;
+            $.ajax({
+                url: deleteUrl,
+                method: 'DELETE',
+                success: function () {
+                    $row.fadeOut('normal', function() {
+                        $(this).remove();
+                        self.updateTotalWeightLifted();
+                    });
+                }
+            })
+        },
+
+        handleRowClick: function() {
+            console.log('Row clicked');
+        },
+
+    };
+
+    /**
+     * A "private" object
+     */
+    var Helper = {
+        initialize: function($wrapper) {
+            this.$wrapper = $wrapper;
+        },
+
+        calculateTotalWeight: function() {
+            var totalWeight = 0;
+            this.$wrapper.find('tbody tr').each(function() {
+                totalWeight += $(this).data('weight');
+            });
+
+            return totalWeight;
+        }
+    };
+})();
